a10e9031716de52bbe4c894e91ee710980ff7784
Jahangir-LyceumHealth <jalam1@lyceumhealth.com>
2022-09-28 19:27:30 -0400

    aws mysql connection

--- connection.js
+++ connection.js
@@ -1,10 +1,22 @@
 const mysql = require("mysql2")
 
+/*
+//LOCAL
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root2022",
     database: "lyceumhealth",
+    multipleStatements: true
+})
+*/
+
+//AWS
+var mysqlConnection = mysql.createConnection({
+    host: "node.cxpwioxnlzt2.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "nodeMySQL2022",
+    database: "LyceumHealth",
     multipleStatements: true
 })
 
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 
 const express = require("express")
 const bodyParser = require("body-parser")
-//const mysqlConnection = require("./connection")
-//const PersonRoutes = require("./routes/person")
+const mysqlConnection = require("./connection")
+const PersonRoutes = require("./routes/person")
 var app = express()
 
-//app.use(bodyParser.json())
-//app.use("/api/person", PersonRoutes)
+app.use(bodyParser.json())
+app.use("/api/person", PersonRoutes)
 
+/*
 app.get("/", (req, res)=>{
     res.send('Welcome to NodeJs home page')
 })
-
+*/
 
 const port = process.env.port || 3000
 app.listen(port)
